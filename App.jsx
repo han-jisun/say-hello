1b5cd58d18d3e426092611e02b5ef174c96a88c2
han-jisun <hanjisun2020@gmail.com>
2023-05-28 23:10:31 +0900

    헤더 수정

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState,useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useNavigation  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as Font from 'expo-font';
+import { AntDesign } from '@expo/vector-icons'; 
 
 import Login from './pages/Login'
 import Join from './pages/Join'
@@ -14,32 +15,42 @@ import List from './pages/NumberList'
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        // 'NotoSans-Regular': require('./assets/fonts/NotoSans-Regular.ttf'),
-      });
-    }
-
-    loadFonts();
-  }, []);
   return (
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <NavigationContainer style={styles.container}>
       <Stack.Navigator initialRouteName="Login"> 
-        <Stack.Screen name="Login" component={Login} 
+        <Stack.Screen name="Login" component={Login}  
           options={{
             headerShown: false,
           }}/>
-        <Stack.Screen name="Join" component={Join} />
-        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="Join" component={Join} options={{ title: '사용자 등록',
+        headerLeft: () => (
+          <BackButton />
+        ), headerTitleAlign: 'center', // 텍스트를 중앙 정렬
+      }} 
+        />
+        <Stack.Screen name="Main" component={Main} options={{ title: '전화걸기' ,headerLeft: () => (
+          <BackButton />
+        ), headerTitleAlign: 'center'}} />
       </Stack.Navigator>
 
     </NavigationContainer>
     
+  );
+}
+function BackButton() {
+  const navigation = useNavigation();
+
+  const handleGoBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <AntDesign
+      name="left"
+      size={24}
+      color="black"
+      onPress={handleGoBack}
+    />
   );
 }
 
--- pages/Join.jsx
+++ pages/Join.jsx
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
 function Join(){
     return(
         <View>
             <Text>가입하기</Text>
+            <AntDesign name="minuscircle" size={24} color="black" />
         </View>
     )
 }
